1783adcbfff01f1295be48fa59726a807438e179
Russ152002 <133365140+Russ152002@users.noreply.github.com>
2023-05-14 14:38:09 +0800

    Add files via upload

---
+++ js/ww.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+
+const burgerMenu = document.querySelector('.navb');
+
+burgerMenu.addEventListener('click', function(){
+
+    const menuContainer = document.querySelector()
+
+});
